e794a538122baa1f4c5b9471802c6ac0a2b539ba
nx111 <gd.zhangdz@gmail.com>
2022-10-07 19:37:17 +0800

    migrate from c language.
    Signed-off-by: nx111 <gd.zhangdz@gmail.com>

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using cp52936;
+using System;
+using System.IO;
+
+FileStream fileStream = new FileStream("test.txt", FileMode.Open);
+BinaryReader binary = new BinaryReader(fileStream);
+byte[] data = binary.ReadBytes((int)fileStream.Length);
+GB18030Encoding encoding = new GB18030Encoding(true);
+string result = encoding.Gb18030_GetString(data);
+Console.WriteLine(result);
